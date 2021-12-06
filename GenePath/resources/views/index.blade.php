9d89902d3e64e6bec77da76c794b4532046c9a90
Emilaberg <emil.aberg@live.com>
2021-12-06 10:36:56 +0100

    update

---
+++ GenePath/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="min-h-screen bg-gray-200 flex flex-col">
+    <div class="mx-auto py-14">
+        <h1 class="text-4xl">GenePath</h1>
+    </div>
+    <div class="mt-auto mx-auto flex flex-row gap-24 py-14">
+        <a href="/create.html" class="text-xl bg-green-400 rounded px-5 py-2 text-green-700 hover:bg-green-300">Create</a>
+        <a href="/load.html" class="text-xl bg-green-400 rounded px-5 py-2 text-green-700 hover:bg-green-300">Load</a>
+    </div>
+</div>
+@endsection

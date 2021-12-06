766556a45ee6554240cee530d55eedeb6190b795
isakjensen <isakludvigjensen@gmail.com>
2021-12-06 09:53:28 +0100

    create interaction done

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 const showShortestPath = document.querySelector("#showShortestPath");
 const ShortestPath = document.querySelector("#shortestPaths");
 
-
 if(showShortestPath && ShortestPath)
 {
     showShortestPath.addEventListener('click', function(){
@@ -9,11 +8,17 @@ if(showShortestPath && ShortestPath)
     });
 }
 
+let WorldTypes = ['circular', 'branch', 'rectangle'];
+
 document.querySelectorAll('input[name="type"]').forEach((element) => {
     element.addEventListener('change', function(event){
-        console.log(event.target.value);
+        let displayElement = document.getElementById(event.target.value);
+
+        for (let i = 0; i < WorldTypes.length; i++)
+        {
+            document.getElementById(WorldTypes[i]).classList.add('hidden');
+        }
+
+        displayElement.classList.remove('hidden');
     });
 });
-
-
-

aca21105bf5bdc5cd5188452387c1fe403b7f8be
isakjensen <isakludvigjensen@gmail.com>
2021-12-09 15:02:36 +0100

    create validation done

--- GenePath/public/scripts/script.js
+++ GenePath/public/scripts/script.js
@@ -10,9 +10,13 @@ if(showShortestPath && ShortestPath)
 
 let WorldTypes = ['circular', 'branch', 'rectangle'];
 
+let WorldType = "";
+
 document.querySelectorAll('input[name="type"]').forEach((element) => {
     element.addEventListener('change', function(event){
         let displayElement = document.getElementById(event.target.value);
+
+        WorldType = event.target.value;
 
         for (let i = 0; i < WorldTypes.length; i++)
         {
@@ -22,3 +26,68 @@ document.querySelectorAll('input[name="type"]').forEach((element) => {
         displayElement.classList.remove('hidden');
     });
 });
+
+const createButton = document.getElementById('createButton');
+
+createButton.addEventListener('click', function(event) {
+
+    // Hide the error badge
+    ErrorElement(false);
+
+    // World name input
+    let worldNameInput = document.querySelector('input[name="worldname"]');
+
+    // Check if name is short
+    if(worldNameInput.value.length < 1)
+            ErrorElement(true, "Error!", "Please enter a world name.");
+
+    if(WorldType === "circular") {
+        let roomInput = document.querySelector('input[name="rooms"]');
+
+        // Check if rooms are empty
+        if(roomInput.value.length < 1)
+            ErrorElement(true, "Error!", "Please enter an amount of rooms.");
+    } else if (WorldType === "branch") {
+        let initRangeInput = document.querySelector('input[name="initrange"]');
+
+        // Check if init range are empty
+        if(initRangeInput.value.length < 1)
+            ErrorElement(true, "Error!", "Please enter an init range.");
+
+        let factorInput = document.querySelector('input[name="factor"]');
+
+        // Check if factor are empty
+        if(factorInput.value.length < 1)
+            ErrorElement(true, "Error!", "Please enter an factor.");
+    } else if (WorldType === "rectangle") {
+        let xroomsInput = document.querySelector('input[name="xrooms"]');
+
+        // Check if x rooms are empty
+        if(xroomsInput.value.length < 1)
+            ErrorElement(true, "Error!", "Please enter an amount of X rooms.");
+
+        let yroomsInput = document.querySelector('input[name="yrooms"]');
+
+        // Check if y rooms are empty
+        if(yroomsInput.value.length < 1)
+            ErrorElement(true, "Error!", "Please enter an amount of Y rooms."); 
+    } else
+        // Show the error badge
+        ErrorElement(true, "Error!", "Please select a world-type.")
+});
+
+function ErrorElement(view, title, content)
+{
+    let errorElement = document.getElementById('errorElement');
+
+    let errorElementContent = document.querySelectorAll('#errorElement div p');
+
+    if(view) {
+        errorElement.classList.remove('hidden');
+
+        errorElementContent[0].textContent = title;
+        errorElementContent[1].textContent = content;
+    } else {
+        errorElement.classList.add('hidden');
+    }
+}
--- GenePath/routes/web.php
+++ GenePath/routes/web.php
@@ -3,11 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\LoadController;
-<<<<<<< HEAD
-=======
 use App\Http\Controllers\CreateController;
 
->>>>>>> 58414bda64638538459ea1f0d73bc674c7f1b4e9
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,15 +28,10 @@ Route::get('/create', function () {
 
 Route::post('/edit', [CreateController::class, 'create']);
 
-// This is the edit page
-Route::get('/edit', function () {
-    return view('edit');
-});
-
 // Shows all the worlds from the database
 Route::get('/load', [LoadController::class, 'ShowWorlds']);
 // Shows all the rooms from the database
-Route::get('/edit', [loadController::class, 'ShowRooms']);
+Route::get('/edit', [LoadController::class, 'ShowRooms']);
 
 // This is the view page
 Route::get('/view', function () {

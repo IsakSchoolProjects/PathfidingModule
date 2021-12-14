730610b9818906bf4cadcc27bfa71b94fd131f19
isakjensen <isakludvigjensen@gmail.com>
2021-12-14 13:07:19 +0100

    createRoom formatted

--- GenePath/public/scripts/createRooms.js
+++ GenePath/public/scripts/createRooms.js
@@ -1,24 +1,22 @@
 // Room generator
 
-// We need to store the Coordinates for each rooms and the we need to paint a representation of that room, 
+// We need to store the Coordinates for each rooms and the we need to paint a representation of that room,
 // That's why we have seperate variables for the painting of the room on the canvas and seperate for the coordinates for the room itself
 
-class world{
-    constructor(name, amount)
-    {
+class world {
+    constructor(name, amount) {
         this.name = name;
         this.rooms = [];
 
-        // Variables for the painting the rooms on the canvas 
+        // Variables for the painting the rooms on the canvas
         const w = 30; //! This is the width of the rooms. MUST be the same as h
         const h = 30; //! This is the height of the rooms. MUST be the same as w
         const spaceBetweenY = 60; //* this is the space between the rooms, MUST be twice the W or H
-        
-        
+
         let x = 30; //! Don't change! same as W
         let y = 30; //! Don't change! same as h
 
-        let currentWidth = 15; //* Should be half as x. Counting the current width and checks if it can fit a room or need to change row 
+        let currentWidth = 15; //* Should be half as x. Counting the current width and checks if it can fit a room or need to change row
 
         // Variables for drawing the line between the rooms
         const startX = 30; //* MUST be the same as x. Resetting X for the canvas
@@ -30,44 +28,40 @@ class world{
         let trueX = 45; // This is the x-coordinate for the each room itself. //! MUST be 1.5x X
         let trueY = 45; //This is the y-coordinate for the each room itself. //! MUST be 1.5x Y
 
-        for(let i=0; i<amount; i++)
-        {
-            let r = new room(i, trueX, trueY)
+        for (let i = 0; i < amount; i++) {
+            let r = new room(i, trueX, trueY);
 
             // Exits
-            if(i==0)
-            {
-                r.exits.push(i+1);
+            if (i == 0) {
+                r.exits.push(i + 1);
 
                 // Draws the path between each rooms offsetting for the center coordinate
                 context.beginPath();
-                context.moveTo(x+offsetStartX, y+offsetY); // offset for rooms width 
-                context.lineTo(x+offsetEndX, y+offsetY); // 
+                context.moveTo(x + offsetStartX, y + offsetY); // offset for rooms width
+                context.lineTo(x + offsetEndX, y + offsetY); //
                 context.stroke();
-
-            }else if(i==amount-1){
-                r.exits.push(i-1);
-            }else{
-                r.exits.push(i-1);
-                r.exits.push(i+1);
+            } else if (i == amount - 1) {
+                r.exits.push(i - 1);
+            } else {
+                r.exits.push(i - 1);
+                r.exits.push(i + 1);
 
                 // Draws the path between each rooms offsetting for the center coordinate
                 context.beginPath();
-                context.moveTo(x+offsetStartX, y+offsetY);
-                context.lineTo(x+offsetEndX, y+offsetY);
+                context.moveTo(x + offsetStartX, y + offsetY);
+                context.lineTo(x + offsetEndX, y + offsetY);
                 context.stroke();
             }
             this.rooms.push(r);
 
             // Painting to the canvas
-            context.fillRect(x,y, w, h);
-            x += w*2; // The coordinate for the next room For the canvas
-            trueX+=w*2 // The true coordinate for the room itself
-            currentWidth += w*2;
+            context.fillRect(x, y, w, h);
+            x += w * 2; // The coordinate for the next room For the canvas
+            trueX += w * 2; // The true coordinate for the room itself
+            currentWidth += w * 2;
 
             // If the width is more than window width start painting on the next row
-            if(currentWidth+w >= window.innerWidth)
-            {
+            if (currentWidth + w >= window.innerWidth) {
                 x = startX;
                 y += spaceBetweenY;
                 trueY += spaceBetweenY;
@@ -78,9 +72,8 @@ class world{
     }
 }
 
-class room{
-    constructor(id, x, y)
-    {
+class room {
+    constructor(id, x, y) {
         this.id = id;
         this.name = roomName();
         this.exits = [];
@@ -88,10 +81,12 @@ class room{
         this.y = y;
     }
 
-    nameGen(){
-        return `A ${array1[Math.floor(Math.random()*array1.length)]} ${array2[Math.floor(Math.random()*array1.length)]} of ${array3[Math.floor(Math.random()*array1.length)]}`; 
+    nameGen() {
+        return `A ${array1[Math.floor(Math.random() * array1.length)]} ${
+            array2[Math.floor(Math.random() * array1.length)]
+        } of ${array3[Math.floor(Math.random() * array1.length)]}`;
     }
-};
+}
 
 let array1 = [
     "earie",
@@ -115,7 +110,7 @@ let array2 = [
     "alley",
     "cave",
     "closet",
-	"cavern",
+    "cavern",
     "dormitory",
 ];
 let array3 = [
@@ -129,53 +124,47 @@ let array3 = [
     "CEO:s",
     "fotballers",
     "clowns",
-    "vampires"
+    "vampires",
 ];
 
-let roomName = function nameGen(){
-    return `A ${array1[Math.floor(Math.random()*array1.length)]} ${array2[Math.floor(Math.random()*array1.length)]} of ${array3[Math.floor(Math.random()*array1.length)]}`; 
+let roomName = function nameGen() {
+    return `A ${array1[Math.floor(Math.random() * array1.length)]} ${
+        array2[Math.floor(Math.random() * array1.length)]
+    } of ${array3[Math.floor(Math.random() * array1.length)]}`;
 };
 
-
-let stringWorld = new world('sewe', 200);
-
+let stringWorld = new world("sewe", 200);
 
 // Rectangular world
 
-class squareWorld{
-    constructor(name, side)
-    {
+class squareWorld {
+    constructor(name, side) {
         this.name = name;
         this.rooms = [];
-        let amount = side*side;
-        for(let i=0; i<amount; i++)
-        {
+        let amount = side * side;
+        for (let i = 0; i < amount; i++) {
             let r = new room(i);
-            let x = i%side;
-            let y = Math.floor(i/side);
-            let temp = []; //exits 
+            let x = i % side;
+            let y = Math.floor(i / side);
+            let temp = []; //exits
             // North
-            if(y>0)
-            {
-                temp.push(i-side);
-            };
+            if (y > 0) {
+                temp.push(i - side);
+            }
             // east
-            if(x<side-1)
-            {
-                temp.push(i+1);
+            if (x < side - 1) {
+                temp.push(i + 1);
             }
             // West
-            if(x>0)
-            {
-                temp.push(i-1);
+            if (x > 0) {
+                temp.push(i - 1);
             }
             // South
-            if(y<side-1)
-            {
-                temp.push(i+side);
+            if (y < side - 1) {
+                temp.push(i + side);
             }
             r.exits = temp;
-            this.rooms.push(r)
+            this.rooms.push(r);
         }
 
         console.log(this.rooms);
@@ -185,76 +174,68 @@ class squareWorld{
 
 // Circular world
 
-class circularWorld{
-    constructor(name, amount, start, end)
-    {
+class circularWorld {
+    constructor(name, amount, start, end) {
         this.name = name;
         this.rooms = [];
-        for(let i=0; i<amount; i++)
-        {
-            let r = new room(i)
-            if(i==0)
-            {
-                r.exits.push(i+1);
-            }else if(i==amount-1){
-                r.exits.push(i-1);
-            }else{
-                r.exits.push(i-1);
-                r.exits.push(i+1);
+        for (let i = 0; i < amount; i++) {
+            let r = new room(i);
+            if (i == 0) {
+                r.exits.push(i + 1);
+            } else if (i == amount - 1) {
+                r.exits.push(i - 1);
+            } else {
+                r.exits.push(i - 1);
+                r.exits.push(i + 1);
             }
             this.rooms.push(r);
         }
-        this.rooms[amount-1].exits.push(0);
-        this.rooms[0].exits.push(amount-1);
+        this.rooms[amount - 1].exits.push(0);
+        this.rooms[0].exits.push(amount - 1);
         console.log(this.rooms);
     }
 }
 
 // let myCircularWorld = new circularWorld('sweden', 10);
 
-
 // SPF - Dans pathfinding som han lade ut
 
-function shortestPath(world,src,dst) {
-    if(world.rooms.length < 2) 
-     {
-         console.log("Omg, you wouldnt be able to find your own ass if the lights went out.");
-         return;
-     }
-    let checking = [[src]];     // this array contains all the paths we are checking
-    let solved = [];            // this array contains all the solved paths so far
+function shortestPath(world, src, dst) {
+    if (world.rooms.length < 2) {
+        console.log(
+            "Omg, you wouldnt be able to find your own ass if the lights went out."
+        );
+        return;
+    }
+    let checking = [[src]]; // this array contains all the paths we are checking
+    let solved = []; // this array contains all the solved paths so far
     let iterations = 0;
     // check each path in checking as long as its not empty
-    while(checking.length>0) {
+    while (checking.length > 0) {
         let newCheck = []; // temporary array to hold newly discovered paths
         // add exits from checking and push them into the array with new-found exits
         // go through the list of known paths to check
-        for(path of checking) {
-            let lastroom = path[path.length-1];
+        for (path of checking) {
+            let lastroom = path[path.length - 1];
             let exits = world.rooms[lastroom].exits;
-            for(let i=0; i<exits.length; i++) {
+            for (let i = 0; i < exits.length; i++) {
                 let temp = [];
                 let exit = world.rooms[lastroom].exits[i];
                 // check if doesn't contain this exit or if we found dst
-                if(exit == dst) {
+                if (exit == dst) {
                     temp = path.slice();
                     temp.push(exit);
                     solved.push(temp.slice());
-                }
-                else if(!path.includes(exit)) {
+                } else if (!path.includes(exit)) {
                     // if not - add it
                     temp = path.slice();
                     temp.push(exit);
                     // push this path to newly discovered paths
-                    newCheck.push(temp.slice());    
-                }
-                else    
-                    ;
+                    newCheck.push(temp.slice());
+                } else;
             }
         }
         checking = newCheck.slice();
     }
     return solved;
 }
-
-
